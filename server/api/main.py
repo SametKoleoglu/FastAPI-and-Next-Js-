6aa66a82f0d21bfd9b12a75f6ca8f7560705fe4c
Samet Koleoglu <sametkoleoglu@192.168.1.3>
2024-08-29 16:54:47 +0300

    Fast API and Next Js

---
+++ server/api/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import auth,workouts,routines
+from .database import Base, engine
+
+
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+     CORSMiddleware,
+     allow_origins=["*"],
+     allow_credentials=True,
+     allow_methods=["*"],
+     allow_headers=["*"],
+)
+
+@app.get("/")
+def health_check():
+     return {"status": "ok"}
+
+
+app.include_router(auth.router)
+app.include_router(workouts.router)
+app.include_router(routines.router)
